948b3dc377b55379206c9496b33f87063df2868c
JerrickJohnson <jerrickjohnson88@gmail.com>
2023-05-17 15:53:23 -0500

    Took out my password

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection(
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'Emerygrace41!$',
+        password: '',
         database: 'employeeTracker_db',
     },
 );
